c1e9e7e098dec99760707c51a12bd69e3f38c816
drigowalves <drigowalves@gmail.com>
2015-05-01 01:29:49 -0300

    Create Aluno.java

---
+++ Aluno.java
@@ -0,0 +1,31 @@
+
+public class Aluno {
+	
+	private String nome, matricula;
+
+	public Aluno(String nome, String matricula){
+		
+		setNome(nome);
+		setMatricula(matricula);
+		
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getMatricula() {
+		return matricula;
+	}
+
+	public void setMatricula(String sobrenome) {
+		this.matricula = sobrenome;
+	}
+	
+	
+	
+}
